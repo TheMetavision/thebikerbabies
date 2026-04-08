968fcf0cc22dca884d471a70b737a44691cbbd69
TheMetavision <contact@themetavision.co.uk>
2026-04-08 17:02:06 +0100

    Remove netlify adapter - static build doesnt need it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-export default defineConfig({ site: 'https://thebikerbabies.com', output: 'static', adapter: netlify(), integrations: [react(), sitemap()], vite: { ssr: { noExternal: ['@sanity/client', '@sanity/image-url'] } } });
+
+export default defineConfig({
+  site: 'https://thebikerbabies.com',
+  output: 'static',
+  integrations: [react(), sitemap()],
+  vite: { ssr: { noExternal: ['@sanity/client', '@sanity/image-url'] } }
+});
